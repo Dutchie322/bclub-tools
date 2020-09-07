7b0c588b60e33be97bc86f2ef35d4b63dd550b29
Wultir <62209995+wultir@users.noreply.github.com>
2020-09-07 20:04:16 +0200

    Don't try to store NPC lovers.

--- projects/background/src/member.ts
+++ projects/background/src/member.ts
@@ -94,7 +94,7 @@ export async function writeFriends(player: IPlayer) {
     }));
   }
   if (player.Lovership) {
-    await Promise.all(player.Lovership.map(async lover => {
+    await Promise.all(player.Lovership.filter(lover => lover.MemberNumber).map(async lover => {
       let member = await retrieveMember(player.MemberNumber, lover.MemberNumber);
       member = Object.assign({}, member, {
         playerMemberNumber: player.MemberNumber,
