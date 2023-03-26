b812f3cf2d618c06baa41de9ba991b8cb7a42878
Dutchie322 <445419+Dutchie322@users.noreply.github.com>
2023-03-26 11:31:22 +0000

    Reduce CPU load when animated assets are used

--- projects/content-script/src/draw-listeners.ts
+++ projects/content-script/src/draw-listeners.ts
@@ -6,43 +6,47 @@ export function characterAppearance(handshake: string) {
 
   const handler = {
     apply(target, thisArg, argumentsList) {
-      if (proxyEnabled) {
-        try {
-          const character = argumentsList[0];
-          if (character && character.MemberNumber) {
-            if (charactersToUpdate[character.MemberNumber]) {
-              clearTimeout(charactersToUpdate[character.MemberNumber]);
-            }
+      const returnValue = target.apply(thisArg, argumentsList);
 
-            charactersToUpdate[character.MemberNumber] = setTimeout(() => {
-              const canvas = character.Canvas as HTMLCanvasElement;
-              const imageData = canvas.toDataURL('image/png');
-              const data = {
-                MemberNumber: character.MemberNumber,
-                CanvasHeight: character.Canvas.height,
-                HeightModifier: character.HeightModifier,
-                HeightRatio: character.HeightRatio,
-                HeightRatioProportion: character.HeightRatioProportion,
-                IsInverted: character.IsInverted(),
-                ImageData: imageData
-              };
-              window.postMessage({
-                handshake,
-                type: 'client',
-                event: 'CommonDrawAppearanceBuild',
-                data,
-              } as IClientMessage<any>, '*');
-
-              delete charactersToUpdate[character.MemberNumber];
-            }, 1000);
-          }
-        } catch (error) {
-          console.warn('[Bondage Club Tools] Could not store character appearance. This is only a problem for the extension, game functionality is not affected.', error);
-          proxyEnabled = false;
-        }
+      if (!proxyEnabled) {
+        return returnValue;
       }
 
-      return target.apply(thisArg, argumentsList);
+      try {
+        const character = argumentsList[0];
+        if (character && character.MemberNumber) {
+          if (charactersToUpdate[character.MemberNumber]) {
+            return returnValue;
+          }
+
+          charactersToUpdate[character.MemberNumber] = setTimeout(() => {
+            const canvas = character.Canvas as HTMLCanvasElement;
+            const imageData = canvas.toDataURL('image/png');
+            const data = {
+              MemberNumber: character.MemberNumber,
+              CanvasHeight: character.Canvas.height,
+              HeightModifier: character.HeightModifier,
+              HeightRatio: character.HeightRatio,
+              HeightRatioProportion: character.HeightRatioProportion,
+              IsInverted: character.IsInverted(),
+              ImageData: imageData
+            };
+            window.postMessage({
+              handshake,
+              type: 'client',
+              event: 'CommonDrawAppearanceBuild',
+              data,
+            } as IClientMessage<any>, '*');
+
+            delete charactersToUpdate[character.MemberNumber];
+          }, 1000);
+        }
+      } catch (error) {
+        console.warn('[Bondage Club Tools] Could not store character appearance. This is only a problem for the extension, game functionality is not affected.', error);
+        proxyEnabled = false;
+      }
+
+      return returnValue;
     }
   } as ProxyHandler<typeof CommonDrawAppearanceBuild>;
   const proxy = new Proxy(CommonDrawAppearanceBuild, handler);
